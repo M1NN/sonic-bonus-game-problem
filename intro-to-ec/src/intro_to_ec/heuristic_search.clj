10ab06c19bb2264a02cab0d3bf5691b7fd1fa863
M1NN <score@morris.umn.edu>
2019-11-25 19:15:05 -0600

    Restarted our implementaion of the sonic-bonus-game-problem and completed it!

--- intro-to-ec/src/intro_to_ec/heuristic_search.clj
+++ intro-to-ec/src/intro_to_ec/heuristic_search.clj
@@ -6,45 +6,33 @@
   [new-states frontier visited]
   (remove (cset/union (set frontier) (set visited)) new-states))
 
-(def heuristic-search
-  {:get-next-node #(first (first %))
-   :add-children #(reduce (fn [front child] (assoc front child (%1 child nil nil))) %2 %3)})
-
 (def a-star-search
   {:get-next-node #(first (first %))
    :add-children #(reduce (fn [front child] (assoc front child (+ (%1 child) %4))) %2 %3)})
-
-(def double-search
-    {:get-next-node #(first (first %))
-     :add-children #(reduce (fn [front child] (assoc front child (+ (%1 child %2 %5) %5))) %3 %4)})
-
-(def breadth-first-search
-  {:get-next-node first
-   :add-children assoc})
-
-(def random-search
-  {:get-next-node rand-nth
-   :add-children concat})
 
 
 (defn generate-path
   [came-from node]
   (if (= :start-node (get came-from node))
     [node]
-    (conj (generate-path came-from (get came-from node)) node)))
+    (conj (generate-path came-from (get came-from node)) (val (first node)))))
 
-(defn search-hs
+
+(defn search-a-star
   [{:keys [get-next-node add-children]}
    {:keys [goal? make-children heuristic]}
    start-node max-calls]
   (loop [frontier (pm/priority-map start-node (heuristic start-node))
+         cost-so-far {start-node 9}
          came-from {start-node :start-node}
          num-calls 0]
-    (println num-calls ": " frontier)
-    (println came-from)
-    (let [current-node (get-next-node frontier)]
+    (println "Number of calls so far is: " num-calls)
+    (let [current-node (get-next-node frontier)
+          new-cost (+ (get cost-so-far current-node) 1)]
+      (println "Our current path: " (generate-path came-from current-node))
       (cond
-        (goal? current-node) (generate-path came-from current-node)
+        (goal? current-node) 
+        (generate-path came-from current-node)
         (= num-calls max-calls) :max-calls-reached
         :else
         (let [kids (remove-previous-states
@@ -53,83 +41,11 @@
            (add-children
             heuristic
             (pop frontier)
-              kids)
+            kids
+            new-cost)
+           (reduce (fn [costs child] (assoc costs child (heuristic child))) cost-so-far kids)
            (reduce (fn [cf child] (assoc cf child current-node)) came-from kids)
-           (inc num-calls)))))))
-(defn search-a-star
- [{:keys [get-next-node add-children]}
-  {:keys [goal? make-children heuristic remove add send-blue send-red]}
-  start-node max-calls]
- (loop [frontier (pm/priority-map start-node (heuristic start-node))
-        cost-so-far {start-node 0}
-        came-from {start-node :start-node}
-        num-calls 0
-        blue-set send-blue
-        red-set send-red]
-   (println num-calls ": " frontier)
-   (println came-from)
-   (println "Cost-so-far is: " cost-so-far)
-   (let [current-node (get-next-node frontier)
-         new-cost (+ (get cost-so-far current-node) 1)]
-     (println "BLUE BALL SET: " blue-set)
-     (cond
-       (goal? blue-set) (generate-path came-from current-node)
-       (= num-calls max-calls) :max-calls-reached
-       :else
-       (let [kids (remove-previous-states
-                   (make-children current-node) frontier (keys came-from))]
-         (println current-node)
-         (if (contains? blue-set current-node)
-           (recur
-            (add-children
-             heuristic
-             (pop frontier)
-             kids
-             new-cost)
-            (reduce (fn [costs child] (assoc costs child (heuristic child))) cost-so-far kids)
-            (reduce (fn [cf child] (assoc cf child current-node)) came-from kids)
-            (inc num-calls)
-            (remove blue-set current-node)
-            (add red-set current-node))
-           
-           (recur
-            (add-children
-             heuristic
-             (pop frontier)
-             kids
-             new-cost)
-            (reduce (fn [costs child] (assoc costs child (heuristic child))) cost-so-far kids)
-            (reduce (fn [cf child] (assoc cf child current-node)) came-from kids)
-            (inc num-calls)
-            (remove blue-set current-node)
-            (add red-set current-node))))))))
+           (inc num-calls)
+           ))))))
 
-(defn search-doubles
- [{:keys [get-next-node add-children]}
-  {:keys [goal? make-children heuristic]}
-  start-node max-calls]
- (loop [frontier (pm/priority-map start-node (heuristic start-node :start-node 0))
-        cost-so-far {start-node 0}
-        came-from {start-node :start-node}
-        num-calls 0]
-   (println num-calls ": " frontier)
-   (println came-from)
-   (println "Cost-so-far is: " cost-so-far)
-   (let [current-node (get-next-node frontier)
-         new-cost (+ (get cost-so-far current-node) 1)]
-     (cond
-       (goal? current-node) (generate-path came-from current-node)
-       (= num-calls max-calls) :max-calls-reached
-       :else
-       (let [kids (remove-previous-states
-                   (make-children current-node) frontier (keys came-from))]
-         (recur
-          (add-children
-           heuristic
-            current-node
-           (pop frontier)
-             kids
-             new-cost)
-          (reduce (fn [costs child] (assoc costs child (heuristic child current-node new-cost))) cost-so-far kids)
-          (reduce (fn [cf child] (assoc cf child current-node)) came-from kids)
-          (inc num-calls)))))))
+
--- intro-to-ec/src/intro_to_ec/sonic_bonus_game_problem.clj
+++ intro-to-ec/src/intro_to_ec/sonic_bonus_game_problem.clj
@@ -1,10 +1,11 @@
 (ns intro-to-ec.sonic-bonus-game-problem)
 
 (defn origin-goal?
-  "A goal checking function that assumes the target
-   position is the origin, i.e., [0 0]."
-  [blue-ball-set]
-  (empty? blue-ball-set))
+  "A goal checking function that checks if the current set of red-balls
+   is equal to the blue-ball-set"
+  [blue-ball-set position]
+  (empty? (clojure.set/difference blue-ball-set
+                                  (clojure.set/intersection blue-ball-set (val (second position))))))
 
 ;; The possible moves in this lattice world. Each
 ;; move is represented by a vector indicating the
@@ -17,9 +18,12 @@
 (def all-moves [up down left right])
 
 (defn apply-move
-  "Apply a move to a given position, yielding the new position"
-  [position move]
-  (vec (map + position move))) ;; TRYING TO FIGURE OUT IFS
+  "Apply a move to a given position, yielding the new position
+   If the new position is in the blue-ball set, add it to the red-ball-set"
+  [blue-ball-set position move]
+  {:postiton (vec (map + (val (first position)) move))
+   :red-balls (if (contains? blue-ball-set (vec (map + (val (first position)) move))) 
+                (conj (val (second position)) (vec (map + (val (first position)) move))) (val (second position)))})
 
 (defn legal-coordinate
   "Limit our search to the space where the given coordinate
@@ -29,29 +33,25 @@
 
 (defn legal-state
   "Return true if both coordinates are legal and this position isn't
-   in the 'wall' set."
+   in the 'red-ball' set."
   [min-range max-range red-ball-set position]
-  (and (every? (partial legal-coordinate min-range max-range) position)
-       (not (contains? red-ball-set position))))
+  (and (every? (partial legal-coordinate min-range max-range) (val (first position)))
+       (not (contains? red-ball-set (val (first position))))))
 
 (defn grid-children
   "Generate a list of all the possible child states
    reachable from the given initial position."
-  [min-range max-range red-ball-set position]
-  (filter (partial legal-state min-range max-range red-ball-set)
-          (map (partial apply-move position) all-moves)))
+  [min-range max-range blue-ball-set position]
+  (filter (partial legal-state min-range max-range (val (second position)))
+          (map (partial apply-move blue-ball-set position) all-moves)))
 
-(defn remove-blue
+(defn blue-left
+  "Our heuristic for this problem, returns the amount of blue-balls
+   minus the amount of red-balls if the position is in the blue-ball-set
+   otherwise return the count of blue-balls"
   [blue-ball-set position]
-  (disj blue-ball-set position))
-
-(defn add-red
-  [red-ball-set position]
-  (conj red-ball-set position))
-
-(defn blue-left [blue-ball-set position]
-  (if (contains? blue-ball-set position)
-    (- (count blue-ball-set) 1) (count blue-ball-set)))
+  (if (contains? blue-ball-set (val (first position)))
+    (- (count blue-ball-set) (count (val (second position)))) (count blue-ball-set)))
 
 (def min-range -2)
 (def max-range 3)
@@ -61,18 +61,27 @@
    (for [x (range -1 2)
          y (range -1 2)]
      [x y])))
+(def side-blues #{[-1 1], [-1 0], [-1 -1], [1 1], [1 0], [1 -1]})
+(def middle-red #{[0 1], [0 0], [0 -1]})
+
+; PROBLEM EXAMPLES
+;
+; This is a simple 3x3, all blues and no reds
+;
+;(hs/search-a-star hs/a-star-search (sonic/make-sonic-problem -2 3 
+;      sonic/blue-3x3) {:position [-1 2], :red-balls #{}} 100)
+;
+; This is a 3x3 where the middle column is all red
+;
+; (hs/search-a-star hs/a-star-search (sonic/make-sonic-problem -2 3 
+;      sonic/side-blue) {:position [-1 2], :red-balls sonic/middle-red} 100)
 
 (defn make-sonic-problem
-  "Create an instance of a simple problem of moving on a grid towards
-   the origin. The ranges specify the bounds on the grid world, and the
-   `wall-set` is a (possibly empty) set of positions that can't be entered
-   or crossed."
-  [min-range max-range red-ball-set blue-ball-set]
-  {:goal? origin-goal?
-   :make-children (partial grid-children min-range max-range red-ball-set)
-   :heuristic (partial blue-left blue-ball-set)
-   :remove (partial remove-blue)
-   :add (partial add-red)
-   :send-blue blue-ball-set
-   :send-red red-ball-set}
+  "Create an instance of a problem of switching all blue-balls on a grid
+   to red-balls"
+  [min-range max-range blue-ball-set]
+  {:goal? (partial origin-goal? blue-ball-set)
+   :make-children (partial grid-children min-range max-range blue-ball-set)
+   :heuristic (partial blue-left blue-ball-set)}
   )
+
